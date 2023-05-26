602f13293fecea07972827de32f7449ae41568f7
Aphelele2 <aphelelejoyi@gmail.com>
2023-05-26 11:22:38 +0200

    done

---
+++ script/code.js
@@ -0,0 +1,13 @@
+function greet(name) {
+    console.log("Hello "+ name);
+}
+greet("Aphelele")
+function multiply(num1,num2) {
+    console.log(num1*num2);
+}
+multiply(5,3)
+function tableOfFive(num) {
+   return num*5  
+}
+let multiplyFive = tableOfFive(15)
+console.log(multiplyFive);
